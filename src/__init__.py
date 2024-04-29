52443ccad1d4dfe2a4adec38f1d2e2d18e7017fe
Srinivas Gorur-Shandilya <code@srinivas.gs>
2024-04-29 10:29:21 -0400

    fix(CLI): better version numbers for installs in dev mode

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+import subprocess
 
 from .do_api import (  # noqa: F401
     cache_do_api_tokens,
@@ -22,3 +23,14 @@ version_filename = os.path.join(SRC_DIR, "VERSION")
 
 with open(version_filename, "r") as file:
     __version__ = file.read()
+
+    if __version__ == "0.0.0.dev0":
+        # in dev mode. we use git to get a "version number"
+        # that will change with tags and commits
+        process = subprocess.run(
+            ["git", "describe", "--tags"],
+            capture_output=True,
+            universal_newlines=True,
+        )
+        if process.returncode == 0:
+            __version__ = process.stdout.strip()
