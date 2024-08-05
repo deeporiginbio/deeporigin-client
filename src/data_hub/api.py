8b519fc5f84737de69a45e8c9988f85b834ccfe4
Srinivas Gorur-Shandilya <code@srinivas.gs>
2024-08-05 14:18:42 -0400

    fix: added type hint

--- src/data_hub/api.py
+++ src/data_hub/api.py
@@ -1037,7 +1037,7 @@ def get_columns(
 
 
 @beartype
-def get_notebook(row_id: str, *, client=None):
+def get_notebook(row_id: str, *, client=None) -> list:
     """Get the body document of a row, if it exists
 
 
