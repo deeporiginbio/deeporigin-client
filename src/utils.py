8627261ebee6fc8f773618dfb058e40ae8925f73
Srinivas Gorur-Shandilya <code@srinivas.gs>
2024-08-07 09:47:52 -0400

    feat: pretty printing of error messages

--- src/utils.py
+++ src/utils.py
@@ -78,6 +78,10 @@ def _truncate(txt: str) -> str:
     """Utility function for truncating text"""
 
     TERMINAL_WIDTH = int(shutil.get_terminal_size().columns / 2)
+
+    if txt is None:
+        return txt
+    txt = str(txt)
     if len(txt) > TERMINAL_WIDTH:
         txt = txt[: TERMINAL_WIDTH - 3] + "..."
     return txt
