1f5f107a0254534077df4f04026e98d92d2a8e6f
Srinivas Gorur-Shandilya <code@srinivas.gs>
2024-09-27 13:13:38 -0400

    fix: changed user_drn -> user_id

--- src/platform/api.py
+++ src/platform/api.py
@@ -5,7 +5,7 @@ from deeporigin import auth
 from deeporigin.config import get_value
 
 
-def resolve_user(user_drn: str):
+def resolve_user(user_id: str):
     """get details about a user in the platform"""
 
     tokens = auth.get_tokens(refresh=False)
@@ -20,18 +20,24 @@ def resolve_user(user_drn: str):
         "cache-control": "no-cache",
     }
 
+    env = get_value()["env"]
+    if env == "prod":
+        url = f"https://os.deeporigin.io/api/organizations/{org_id}/users/{user_id}"
+    else:
+        f"https://{env}.deeporigin.io/api/organizations/{org_id}/users/{user_id}"
+
     response = requests.get(
-        f"https://os.deeporigin.io/api/organizations/{org_id}/users/{user_drn}",
+        url,
         headers=headers,
     )
 
     return response.json()
 
 
-def get_user_name(user_drn: str):
+def get_user_name(user_id: str):
     """get user name from user DRN"""
 
-    response = resolve_user(user_drn)
+    response = resolve_user(user_id)
 
     return response["data"]["attributes"]["name"]
 
@@ -42,7 +48,7 @@ def get_last_edited_user_name(row):
 
     if row.edited_by_user_drn is None:
         # fall back to created by user
-        user_drn = row.created_by_user_drn.split(":")[-1]
+        user_id = row.created_by_user_drn.split(":")[-1]
     else:
-        user_drn = row.edited_by_user_drn.split(":")[-1]
-    return get_user_name(user_drn)
+        user_id = row.edited_by_user_drn.split(":")[-1]
+    return get_user_name(user_id)
