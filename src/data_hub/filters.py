2615ae470ea5e6adb8d5bc147da49612bdfdbd22
Srinivas Gorur-Shandilya <code@srinivas.gs>
2024-12-12 16:15:50 -0500

    fix: added docstrings

--- src/data_hub/filters.py
+++ src/data_hub/filters.py
@@ -33,6 +33,16 @@ def filter(
     filter_value: Any,
     column_id: str,
 ):
+    """function to create filters and conditions to filter rows in databases on the data hub
+
+    Args:
+        filter_type (FilterType): type of filter. can be one of number, text, boolean
+        operator (Operator): operator to apply
+        filter_value (Any): value of filter
+        column_id (str): id or name of column
+
+
+    """
     data = dict(
         column_id=column_id,
         filter_type=filter_type,
