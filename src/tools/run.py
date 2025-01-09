b3dabec2669a27e633c036189da39850b7464801
Areg Sarinyan <35684826+aregsyan@users.noreply.github.com>
2025-01-09 06:09:10 +0400

    Draco (#133)
    Co-authored-by: vdelannee <vdelannee@deeporigin.com>
    Co-authored-by: Srinivas Gorur-Shandilya <code@srinivas.gs>

--- src/tools/run.py
+++ src/tools/run.py
@@ -245,6 +245,55 @@ def autodock_vina(
 
 
 @beartype
+def draco(
+    *,
+    database_id: str,
+    row_id: str,
+    input_file_column_name: str,
+    output_column_name: str,
+) -> str:
+    """starts an Draco run
+
+    Args:
+        database_id (str): database ID or name of the database to source inputs from and write outputs to
+        row_id (str): row ID or name of the row to source inputs from and write outputs to.
+        input_file_column_name (str): name of the column to source the input file from
+        output_column_name (str): name of the column to write output file to
+
+    Returns:
+        str: job ID of the run
+
+
+    """
+
+    TOOL_ID = "deeporigin/draco"
+
+    inputs = dict(
+        input_file={
+            "rowId": row_id,
+            "columnId": input_file_column_name,
+            "databaseId": database_id,
+        },
+    )
+    outputs = dict(
+        output_file={
+            "rowId": row_id,
+            "columnId": output_column_name,
+            "databaseId": database_id,
+        },
+    )
+
+    db = api.describe_database(database_id=database_id)
+
+    return _process_job(
+        cols=db.cols,
+        inputs=inputs,
+        outputs=outputs,
+        tool_id=TOOL_ID,
+    )
+
+
+@beartype
 def _process_job(
     *,
     inputs: dict,
