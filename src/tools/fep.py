ca2ebce487646ff79d2a1213852a7ab41b94882c
Srinivas Gorur-Shandilya <code@srinivas.gs>
2025-03-03 15:01:57 -0500

    feat: ability to run end to end abfe

--- src/tools/fep.py
+++ src/tools/fep.py
@@ -11,8 +11,22 @@ import pandas as pd
 from beartype import beartype
 from box import Box
 from deeporigin import chemistry
+from deeporigin.data_hub import api
+from deeporigin.exceptions import DeepOriginException
+from deeporigin.tools import run
 from deeporigin.tools.toolkit import _ensure_columns, _ensure_database
 from IPython.display import HTML, display
+
+# constants
+DB_ABFE = "ABFE2"
+DB_PROTEINS = "Proteins"
+DB_LIGANDS = "Ligands"
+COL_PROTEIN = "Protein"
+COL_LIGAND = "Ligand"
+COL_STEP = "Step"
+COL_JOBID = "JobID"
+COL_OUTPUT = "OutputFile"
+COL_RESULT = "ResultFile"
 
 
 class PrettyDict(Box):
@@ -102,62 +116,18 @@ class Protein:
             self.name = self.file.name
 
 
-@beartype
-def _ensure_dbs_for_fep():
-    """ensure that there are databases for FEP on the data hub
-
-    Ensures the following DBs:
-
-    - ligand (list of ligand files)
-    - protein (list of protein files)
-    - ABFE (ligand, protein, step, job_id, output, results, delta_g)
-    - RBFE (ligand1, ligand2, protein, step, job_id, output, results, delta_g)
-
-    """
-
-    # ligands
-    database = _ensure_database("Ligands")
-    required_columns = [
-        dict(name="Ligand", type="file"),
-    ]
-    _ensure_columns(
-        database=database,
-        required_columns=required_columns,
-    )
-
-    # proteins
-    database = _ensure_database("Proteins")
-    required_columns = [
-        dict(name="Protein", type="file"),
-    ]
-    _ensure_columns(
-        database=database,
-        required_columns=required_columns,
-    )
-
-    # ABFE
-    database = _ensure_database("ABFE")
-    required_columns = [
-        dict(name="Protein", type="text"),
-        dict(name="Ligand", type="text"),
-        dict(name="Step", type="text"),
-        dict(name="JobID", type="text"),
-        dict(name="OutputFile", type="file"),
-        dict(name="ResultFile", type="file"),
-        dict(name="delta_g", type="file"),
-    ]
-    _ensure_columns(
-        database=database,
-        required_columns=required_columns,
-    )
-
-
 @dataclass
 class FEP:
     """class for FEP simulations"""
 
     ligands: list[Ligand]
     protein: Protein
+
+    _ligands_db: Optional[dict] = None
+    _proteins_db: Optional[dict] = None
+    _abfe_db: Optional[dict] = None
+
+    params_abfe_end_to_end = _load_params("abfe_end_to_end")
 
     @classmethod
     def from_dir(cls, directory: str) -> "FEP":
@@ -196,6 +166,58 @@ class FEP:
 
         return fep
 
+    @beartype
+    def _ensure_dbs_for_fep(self):
+        """ensure that there are databases for FEP on the data hub
+
+        Ensures the following DBs:
+
+        - ligand (list of ligand files)
+        - protein (list of protein files)
+        - ABFE (ligand, protein, step, job_id, output, results, delta_g)
+        - RBFE (ligand1, ligand2, protein, step, job_id, output, results, delta_g)
+
+        """
+
+        # ligands
+        database = _ensure_database(DB_LIGANDS)
+        required_columns = [
+            dict(name=COL_LIGAND, type="file"),
+        ]
+        _ensure_columns(
+            database=database,
+            required_columns=required_columns,
+        )
+        self._ligands_db = database
+
+        # proteins
+        database = _ensure_database(DB_PROTEINS)
+        required_columns = [
+            dict(name="Protein", type="file"),
+        ]
+        _ensure_columns(
+            database=database,
+            required_columns=required_columns,
+        )
+        self._proteins_db = database
+
+        # ABFE
+        database = _ensure_database(DB_ABFE)
+        required_columns = [
+            dict(name=COL_PROTEIN, type="text"),
+            dict(name=COL_LIGAND, type="text"),
+            dict(name=COL_STEP, type="text"),
+            dict(name=COL_JOBID, type="text"),
+            dict(name=COL_OUTPUT, type="file"),
+            dict(name=COL_RESULT, type="file"),
+            dict(name="delta_g", type="file"),
+        ]
+        database = _ensure_columns(
+            database=database,
+            required_columns=required_columns,
+        )
+        self._abfe_db = database
+
     def show_ligands(self):
         """show ligands in a dataframe"""
 
@@ -220,5 +242,154 @@ class FEP:
     def connect(self):
         """connects the local instantiation of the simulation to Deep Origin"""
 
+        self._ensure_dbs_for_fep()
+
         # ensure that ligands are uploaded
-        _ensure_dbs_for_fep()
+        df = api.get_dataframe(DB_LIGANDS)
+
+        for ligand in self.ligands:
+            ligand_file = os.path.basename(ligand.file)
+            matching_indices = df.index[df[COL_LIGAND] == ligand_file].tolist()
+            if len(matching_indices) == 0:
+                print(f"Uploading {ligand.file}...")
+                response = api.upload_file_to_new_database_row(
+                    database_id=DB_LIGANDS,
+                    column_id=COL_LIGAND,
+                    file_path=str(ligand.file),
+                )
+
+                ligand._do_id = response.rows[0].hid
+            else:
+                ligand._do_id = matching_indices[0]
+
+        # ensure that protein is uploaded
+        df = api.get_dataframe(DB_PROTEINS)
+        protein_file = os.path.basename(self.protein.file)
+        matching_indices = df.index[df[COL_PROTEIN] == protein_file].tolist()
+        if len(matching_indices) == 0:
+            print(f"Uploading {self.protein.file}...")
+            response = api.upload_file_to_new_database_row(
+                database_id=DB_PROTEINS,
+                column_id=COL_PROTEIN,
+                file_path=str(self.protein.file),
+            )
+
+            self.protein._do_id = response.rows[0].hid
+        else:
+            self.protein._do_id = matching_indices[0]
+
+    def abfe_end_to_end(
+        self,
+        *,
+        ligand_ids: Optional[str] = None,
+    ) -> str:
+        """Function to run an end-to-end job"""
+
+        if ligand_ids is None:
+            raise NotImplementedError("Not implemented yet")
+
+        # check that ligand IDs are valid
+        valid_ligand_ids = [ligand._do_id for ligand in self.ligands]
+
+        if None in valid_ligand_ids:
+            raise DeepOriginException(
+                "Some ligands have not been uploaded yet. Use .connect() first."
+            )
+
+        if not set(ligand_ids).issubset(valid_ligand_ids):
+            raise DeepOriginException(
+                f"Some ligand IDs re not valid. Valid ligand IDs are: {valid_ligand_ids}"
+            )
+
+        database_columns = (
+            self._ligands_db.cols + self._proteins_db.cols + self._abfe_db.cols
+        )
+
+        for ligand_id in ligand_ids:
+            start_abfe_run_and_log(
+                protein_id=self.protein._do_id,
+                ligand_id=ligand_id,
+                database_columns=database_columns,
+                params=self.params_abfe_end_to_end,
+            )
+
+
+def start_abfe_run_and_log(
+    *,
+    protein_id: str,
+    ligand_id: str,
+    params: dict,
+    database_columns: list,
+):
+    """starts a single run of ABFE end to end and logs it in the ABFE database"""
+
+    tool_key = "deeporigin.abfe-end-to-end"
+
+    # make a new row
+    response = api.make_database_rows(DB_ABFE, n_rows=1)
+    row_id = response.rows[0].hid
+
+    # write ligand ID
+    api.set_cell_data(
+        ligand_id,
+        column_id=COL_LIGAND,
+        row_id=row_id,
+        database_id=DB_ABFE,
+    )
+
+    # write protein ID
+    api.set_cell_data(
+        protein_id,
+        column_id=COL_PROTEIN,
+        row_id=row_id,
+        database_id=DB_ABFE,
+    )
+
+    # write step
+    api.set_cell_data(
+        "End-to-end",
+        column_id=COL_STEP,
+        row_id=row_id,
+        database_id=DB_ABFE,
+    )
+
+    # start job
+    params["protein"] = {
+        "columnId": "Protein",
+        "rowId": protein_id,
+        "databaseId": "Proteins",
+    }
+
+    params["ligand"] = {
+        "columnId": COL_LIGAND,
+        "rowId": ligand_id,
+        "databaseId": DB_LIGANDS,
+    }
+
+    outputs = {
+        "output_file": {
+            "columnId": COL_OUTPUT,
+            "rowId": row_id,
+            "databaseId": DB_ABFE,
+        },
+        "abfe_results_summary": {
+            "columnId": COL_RESULT,
+            "rowId": row_id,
+            "databaseId": DB_ABFE,
+        },
+    }
+
+    job_id = run._process_job(
+        inputs=params,
+        outputs=outputs,
+        tool_key=tool_key,
+        cols=database_columns,
+    )
+
+    # write job ID
+    api.set_cell_data(
+        job_id,
+        column_id=COL_JOBID,
+        row_id=row_id,
+        database_id=DB_ABFE,
+    )
