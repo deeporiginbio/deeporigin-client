5edd8eacf4f37233f4c38381ea3679fb3fec11b5
Srinivas Gorur-Shandilya <code@srinivas.gs>
2025-04-08 10:40:04 -0400

    fix: import sort

--- src/drug_discovery/__init__.py
+++ src/drug_discovery/__init__.py
@@ -6,14 +6,13 @@ molecule manipulation, protein-ligand interactions, and computational chemistry
 calculations.
 """
 
-from . import chemistry
-
 # example data
 import asyncio  # noqa: F401
 from importlib.resources import path
 
 import nest_asyncio  # we need this for asyncio  # noqa: F401
 
+from . import chemistry
 from .structures import Ligand, Pocket, Protein
 
 __all__ = ["chemistry", "Complex", "Protein", "Ligand", "Pocket"]
