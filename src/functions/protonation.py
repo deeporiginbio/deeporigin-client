6f20852fd13c5d1e317815c79833ef48f1948d25
Srinivas Gorur-Shandilya <code@srinivas.gs>
2025-08-26 10:39:23 -0400

    feat: protonation function

---
+++ src/functions/protonation.py
@@ -0,0 +1,75 @@
+"""This module implements a low level function to perform molecular property predictions
+using the DeepOrigin API.
+"""
+
+import json
+import os
+from pathlib import Path
+
+from beartype import beartype
+import requests
+
+from deeporigin.utils.core import hash_dict
+
+URL = "http://molprops.default.jobs.edge.deeporigin.io/protonation"
+CACHE_DIR = os.path.expanduser("~/.deeporigin/protonation")
+
+# Ensure cache directory exists
+os.makedirs(CACHE_DIR, exist_ok=True)
+
+
+@beartype
+def protonate(
+    *,
+    smiles: str,
+    ph: float = 7.4,
+    filter_percentage: float = 1.0,
+    use_cache: bool = True,
+) -> dict:
+    """
+    Run molecular property prediction using the DeepOrigin API.
+
+    Args:
+        smiles_string (str): SMILES string for the molecule
+        use_cache (bool): Whether to use the cache
+
+    Returns:
+        str: Path to the cached SDF file containing the results
+    """
+
+    payload = {
+        "smiles": smiles,
+        "ph": ph,
+        "filter_percentage": filter_percentage,
+    }
+
+    # Create hash of inputs
+    cache_hash = hash_dict(payload)
+    response_file = str(Path(CACHE_DIR) / f"{cache_hash}.json")
+
+    # Check if cached result exists
+    if os.path.exists(response_file) and use_cache:
+        # Read cached response
+        with open(response_file, "r") as file:
+            response = json.load(file)
+
+    else:
+        # Prepare the request payload
+
+        # Make the API request
+        response = requests.post(
+            URL,
+            json=payload,
+            headers={"Content-Type": "application/json"},
+        )
+
+        # Raise an exception for bad status codes
+        response.raise_for_status()
+
+        response = response.json()
+
+        # Write JSON response to cache
+        with open(response_file, "w") as file:
+            json.dump(response, file)
+
+    return response
